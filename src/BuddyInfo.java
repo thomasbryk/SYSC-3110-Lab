2094317568ca2d0b56d80d702153da72d0157700
thomasbryk <thomasbryk@cb5107-32.labs.sce.carleton.ca>
2017-10-02 09:44:01 -0400

    Initial File

---
+++ src/BuddyInfo.java
@@ -0,0 +1,41 @@
+
+public class BuddyInfo {
+	private String name;
+	private String homeAddress;
+	private String phoneNumber;
+	
+	public BuddyInfo (String name, String homeAddress, String phoneNumber) {
+		super();
+		this.name=name;
+		this.homeAddress=homeAddress;
+		this.phoneNumber=phoneNumber;
+	}
+	
+	public static void main (String[] args) {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName (String name) {
+		this.name=name;
+	}
+	
+	public String getHomeAddress() {
+		return this.homeAddress;
+	}
+	
+	public void setHomeAddress(String homeAddress) {
+		this.homeAddress=homeAddress;
+	}
+	
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+	
+	public void setPhoneNumber (String phoneNumber) {
+		this.phoneNumber=phoneNumber;
+	}
+}
